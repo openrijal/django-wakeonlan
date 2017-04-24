ba4d8ee9d18670941b11fb929e454667830c3b3b
Nitesh Rijal <rijal.it@gmail.com>
2017-04-24 14:01:42 +0545

    add allowed hosts

--- lanwake/settings.py
+++ lanwake/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'ew^06#ph*^po5(o^8%ixf#f^ye(3@9k^lv=op&ckk2zt%t2vo4'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
